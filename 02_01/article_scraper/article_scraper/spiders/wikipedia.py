bc1c7b8cf4f551f630ab30fe3592ba018e94a786
Martín Chiri <43660072+MartinChiri@users.noreply.github.com>
2022-12-14 15:46:57 +0000

    all fine until 2.3

--- 02_01/article_scraper/article_scraper/spiders/wikipedia.py
+++ 02_01/article_scraper/article_scraper/spiders/wikipedia.py
@@ -13,11 +13,11 @@ class WikipediaSpider(CrawlSpider):
     custom_settings = {
         'FEED_URI' : 'articles.xml',
         'FEED_FORMAT': 'xml'
-    }
-    
+    }   
     def parse_info(self, response):
         article = Article()
-        article['title'] = response.xpath('//h1/i/text()').get() or response.xpath('//h1/span/text()').get()
+        article['title'] = response.xpath('//h1/i/text()').get() \
+        or response.xpath('//h1/span/text()').get()
         article['url'] = response.url
         article['lastUpdated'] = response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
         return article
