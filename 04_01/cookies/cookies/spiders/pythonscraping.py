2173e6228818a4d2379a359a7a92548ce7ec3cf8
Martín Chiri <43660072+MartinChiri@users.noreply.github.com>
2022-12-15 18:33:46 +0000

    04_01 done

--- 04_01/cookies/cookies/spiders/pythonscraping.py
+++ 04_01/cookies/cookies/spiders/pythonscraping.py
@@ -5,7 +5,14 @@ import scrapy
 class PythonscrapingSpider(scrapy.Spider):
     name = 'pythonscraping'
     allowed_domains = ['pythonscraping.com']
-    start_urls = ['http://pythonscraping.com/']
+    start_urls = ['http://pythonscraping.com/linkedin/cookies/profile.php']
+
+
+    def make_requests_from_url(self, url):
+        request = super(PythonscrapingSpider, self).make_requests_from_url(url)
+        request.cookies['username'] = 'Jose'
+        request.cookies['loggedin'] = 1
+        return request
 
     def parse(self, response):
-        pass
+        return {'text': response.xpath('//body/text()').get()}
